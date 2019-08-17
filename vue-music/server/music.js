63c0116705ca38d60b135b1e4f2fe861f3a1e296
kkboom <1396956549@qq.com>
2019-08-17 18:53:55 +0800

    add playlist api

--- vue-music/server/music.js
+++ vue-music/server/music.js
@@ -1,265 +1,147 @@
-const Koa = require('koa');
-const fs = require('fs');
-const Router = require('koa-router');
-const static = require('koa-static');
-const KoaBody = require('koa-body');
-const config = require('./config');
+const Koa = require("koa");
+const Router = require("koa-router");
+const rp = require("request-promise");
+const db = require("./db");
 const app = new Koa();
-const uuidv1 = require('uuid/v1');
-
-app.context.db = config.db;
-app.use(async (ctx, next) => {
-    ctx.set('Access-Control-Allow-Origin', '*');
-    ctx.set('Access-Control-Allow-Headers', '*');
-    await next();
-});
-
-// 静态资源访问地址
-// app.use(static(config.BASE_PATH));
-app.use(KoaBody({
-    multipart: true,
-    formidable: {
-        maxFieldsSize: 50 * 1024 * 1024  //50M
-    }
-}));
-
 const router = new Router();
-app.use(router.routes()).use(router.allowedMethods());
 
-/**
- * 测试接口
- */
-router.get('/', async ctx => {
-    ctx.body = { msg: "success" };
-})
+app.context.db = db.news;
 
-
-/**
- * @desc song upload api
- * @port /upload/, public
- * @param ...
- */
-router.post('/upload', async ctx => {
-    try {
-        const files = ctx.request.files;
-        const song = files.music || '';
-        const picture = files.picture || '';
-        const data = ctx.request.body;
-        // console.log(data);
-        // console.log(song);
-
-        // 先存音乐背景图片，返回url地址
-        let picurl = ''
-        if (picture.size > 0 && picture.size < 5242880) {
-            const newFileName = await saveFile(picture, config.MUSIC_PIC)
-            picurl = newFileName ? (config.PIC_SERVER_NAME + newFileName) : config.DEFAULT_PIC
-        } else {
-            picurl = config.DEFAULT_PIC
-        }
-        // 判断歌曲大小
-        if (song.size > 0 && song.size < 52428800) {
-            const title = data.title || song.name
-            const mid = uuidv1()
-            const singer = data.singer || '未知歌手'
-            const album = data.album || '未知专辑'
-            const size = song.size
-            const text = data.text || '暂无歌词'
-
-            const newFileName = await saveFile(song, config.MUSIC_PATH)
-            const purl = newFileName ? (config.MUSIC_SERVER_NAME + newFileName) : ''
-            // 整理要写入到数据库的数据
-            const item = [title, mid, singer, album, size, picurl, purl, text];
-
-            // 存储相关信息到数据库
-            const savaSong = await ctx.db.query(
-                "insert into songs set title=?,mid=?,singer=?,album=?,msize=?,picurl=?,purl=?,mtext=?",
-                item
-            );
-
-            if (savaSong.affectedRows > 0) {
-                ctx.status = 200;
-                ctx.body = { msg: '歌曲文件上传成功' };
-            } else {
-                ctx.status = 200;
-                ctx.body = { msg: '上传歌曲文件失败，请稍后再试' };
-            }
-        } else {
-            ctx.status = 200;
-            ctx.body = { msg: '歌曲文件大于50M或格式有误' };
-        }
-    } catch (e) {
-        console.log(e)
-        ctx.status = 500;
-        ctx.body = { msg: 'server error' }
-    }
+app.use(async (ctx, next) => {
+  ctx.set("Access-Control-Allow-Headers", "*");
+  ctx.set("Access-Control-Allow-Origin", "*");
+  await next();
 });
 
+// 获取qq音乐vkey
+router.get("/qqmusic", async ctx => {
+  try {
+    const id = ctx.query.id.toString();
+    const preurl =
+      "https://u.y.qq.com/cgi-bin/musicu.fcg?-=getplaysongvkey4583585168216277&g_tk=377014325&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=0&data=";
 
-/**
- * @desc get songlist api
- * @port /songlist, public
- * @param page
- */
-router.get('/songlist', async ctx => {
-    try {
-        const num = parseInt(ctx.query.num) || 100;
-        const singer = ctx.query.singer.toString()
-        // num合法
-        let re = /^[\u4e00-\u9fa5a-zA-Z0-9]+$/gi
-        if (num >= 0 && re.test(singer)) {
-            const songlist = await ctx.db.query(
-                "select * from songs where singer=? order by add_date desc limit ?",
-                [singer, num]
-            );
-            if (songlist.length > 0) {
-                ctx.status = 200;
-                ctx.body = { msg: 'success', songlist };
-            }
-        } else {
-            ctx.status = 200;
-            ctx.body = { msg: '参数不合法' };
-        }
-    } catch (e) {
+    const data = `{"req_0":{"module":"vkey.GetVkeyServer","method":"CgiGetVkey","param":{"guid":"1411302730","songmid":["${id}"],"songtype":[0],"uin":"1396956549","loginflag":1,"platform":"20"}},"comm":{"uin":1396956549,"format":"json","ct":24,"cv":0}}`;
+
+    const returl = preurl + encodeURIComponent(data);
+
+    await rp(returl)
+      .then(res => {
+        const jsonres = JSON.parse(res);
+        const resdata = jsonres.req_0.data;
+        ctx.body = { midurlinfo: resdata.midurlinfo[0], sip: resdata.sip };
+      })
+      .catch(e => {
         console.log(e);
-        ctx.status = 500;
-        ctx.body = { msg: 'server error' };
-    }
-})
+      });
+  } catch (e) {
+    console.log(e);
+    ctx.body = {
+      msg: "error"
+    };
+  }
+});
 
-/**
- * @desc del fileitem api
- * @port /api/del/, private
- * @query id
- */
-router.get('/del', async ctx => {
-    try {
-        const id = ctx.query.id;
-        const token = ctx.headers.cookie;
-        if (token) {
-            await jwt.verify(token, config.SECRET, async (err, decode) => {
-                // token验证通过
-                console.log(decode);
-                if (decode) {
-                    const user = await ctx.db.query(
-                        'select * from user where email = ?', decode.email
-                    );
-                    if (user.length > 0) {
-                        const delResult = await ctx.db.query(
-                            "update file set showable=0 where id = ?", id
-                        );
-                        if (delResult.affectedRows > 0) {
-                            ctx.status = 200;
-                            ctx.body = { msg: '删除成功' };
-                        } else {
-                            ctx.status = 200;
-                            ctx.body = { msg: '删除失败' };
-                        }
-                    }
-                }
-            })
-        } else {
-            ctx.status = 400;
-            ctx.body = { msg: '未登录或没有权限' };
-        }
-    } catch (e) {
-        ctx.status = 500;
-        ctx.body = { msg: 'err' };
-    }
-})
+// 获取qq音乐歌词接口
+router.get("/musicLyric", async ctx => {
+  try {
+    const id = ctx.query.id.toString();
+    const preurl =
+      "https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg?-=MusicJsonCallback_lrc&pcachetime=1565861980177&g_tk=693935862&loginUin=1396956549&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=0";
 
-/**
- * @desc search file
- * @port /api/find/, public
- * @query keywords
- */
-router.get('/find', async ctx => {
-    try {
-        const keywords = ctx.query.k;
-        let re = /^[\u4e00-\u9fa5a-zA-Z0-9]+$/gi
-        if (re.test(keywords)) {
-            const findResult = await ctx.db.query(
-                'select * from songs where title like ? or singer like ? order by add_date desc',
-                ['%' + keywords + '%', '%' + keywords + '%']
-            );
-            if (findResult.length > 0) {
-                ctx.status = 200;
-                ctx.body = { msg: 'success', findResult };
-            } else {
-                ctx.status = 200;
-                ctx.body = { msg: '没有搜索结果' };
-            }
-        } else {
-            ctx.status = 400;
-            ctx.body = { msg: '参数不合法' };
-        }
+    var options = {
+      uri: preurl,
+      qs: {
+        songmid: id
+        //   &=002qpjAV2lYx81
+      },
+      headers: {
+        "User-Agent": "Request-Promise",
+        referer: "https://y.qq.com/portal/player.html"
+      },
+      json: true // Automatically parses the JSON string in the response
+    };
 
-    } catch (e) {
-        ctx.status = 500;
-        ctx.body = { msg: 'server error' };
-    }
-})
-
-/**
- * @desc admin login api
- * @port /api/login/,public
- * @params email,passwd
- */
-router.post('/login', async ctx => {
-    try {
-        const data = ctx.request.body;
-        // 数据验证
-        // 查找用户
-        const findUser = await ctx.db.query(
-            'select * from user where email = ?',
-            data.email
-        );
-        if (findUser.length > 0) {
-            const user = findUser[0];
-            const payLoad = {
-                email: user.email,
-                name: user.name
-            }
-            await bcrypt.compare(data.passwd, user.passwd).then(res => {
-                if (res) {
-                    // 生成token
-                    var token = jwt.sign(payLoad, config.SECRET, { expiresIn: 3600 });
-                    ctx.status = 200;
-                    ctx.body = { msg: '管理员身份登录成功！', user: payLoad, token };
-                } else {
-                    ctx.status = 400;
-                    ctx.body = { msg: '密码错误！' };
-                }
-            });
-        } else {
-            ctx.status = 400;
-            ctx.body = { msg: '用户不存在！' };
-        }
-    } catch (e) {
+    await rp(options)
+      .then(res => {
+        ctx.body = { code: res.code, lyric: res.lyric };
+      })
+      .catch(e => {
         console.log(e);
-        ctx.status = 500;
-        ctx.body = { msg: 'err' };
-    }
-})
+      });
+  } catch (e) {
+    console.log(e);
+    ctx.body = {
+      msg: "error"
+    };
+  }
+});
 
+router.get("/playlist", async ctx => {
+  try {
+    const preurl =
+      "https://u.y.qq.com/cgi-bin/musicu.fcg?-=recom49029912403646314&g_tk=693935862&loginUin=1396956549&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=0&data=";
 
-const port = process.env.PORT || 9090;
-app.listen(port, () => {
-    console.log(`app is running at port ${port}`);
-})
+    const data =
+      '{"comm":{"ct":24},"playlist":{"method":"get_playlist_by_category","param":{"id":3317,"curPage":1,"size":40,"order":5,"titleid":3317},"module":"playlist.PlayListPlazaServer"}}';
 
-async function saveFile(file, saveDir) {
-    // 创建可读流
-    const rs = fs.createReadStream(file.path);
-    // 重命名(uuid)
-    const type = file.name.split('.').pop()
-    const newFileName = uuidv1() + '.' + type
-    // 创建可写流
-    const ws = fs.createWriteStream(saveDir + newFileName);
-    // 管道链接传输并获取地址
-    const file_path = await rs.pipe(ws).path
-    if (file_path) {
-        return newFileName
-    } else {
-        return ''
-    }
-}
+    const returl = preurl + encodeURIComponent(data);
+
+    await rp(returl)
+      .then(res => {
+        const jsonres = JSON.parse(res);
+        if (jsonres.code === 0) {
+          ctx.body = { msg: "sucess", playlist: jsonres.playlist.data };
+        } else {
+          ctx.body = { msg: "error" };
+        }
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  } catch (error) {
+    throw error;
+  }
+});
+
+// 获取歌单详情接口
+router.get("/cddetail", async ctx => {
+  try {
+    const id = ctx.query.id.toString();
+    const preurl =
+      "https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg?type=1&json=1&utf8=1&onlysong=0&new_format=1&g_tk=693935862&loginUin=1396956549&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=0";
+
+    var options = {
+      uri: preurl,
+      qs: {
+        disstid: id
+        // disstid=7001035571
+      },
+      headers: {
+        "User-Agent": "Request-Promise",
+        referer: `https://y.qq.com/n/yqq/playsquare/${id}.html`
+      },
+      json: true // Automatically parses the JSON string in the response
+    };
+
+    await rp(options)
+      .then(res => {
+        if (res.code === 0) {
+          ctx.body = { code: res.code, cdlist: res.cdlist };
+        } else {
+          ctx.body = { code: res.code };
+        }
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  } catch (e) {
+    ctx.body = {
+      msg: "error"
+    };
+  }
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+app.listen(9999, () => {
+  console.log("app id running on port 9999");
+});
